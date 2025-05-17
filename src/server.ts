630f70e88f914400a4fccd2ea0780b40f8051cac
Levie-Asus <svpv.hotro@gmail.com>
2025-05-17 11:21:55 +0700

    feat: enhance WebSocket connection handling and reload logic

--- src/server.ts
+++ src/server.ts
@@ -11,12 +11,21 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.static(path.join(__dirname, "../dist")));
 
+let hasReloaded = false;
+
 wss.on("connection", (ws) => {
-  console.log("Client connected");
-  setTimeout(() => {
-    if (ws.readyState === 1) ws.send("reload");
-  }, 100);
+  console.log("Client connected!");
+  if (!hasReloaded) {
+    hasReloaded = true;
+    setTimeout(() => {
+      for (const client of wss.clients) {
+        if (client.readyState === 1) client.send("reload");
+      }
+    }, 100);
+  }
 });
+
+
 
 server.listen(PORT, () => {
   console.log(`Server running: http://localhost:${PORT}`);
