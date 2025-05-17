7ffa41896a1a0abe6160b8c5278ead8fef7c1a81
Levie-Asus <svpv.hotro@gmail.com>
2025-05-17 15:36:08 +0700

    feat: add index.ts to export core and entity components

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// core
+export * from "./core/game"
+export * from "./core/scene"
+export * from "./core/sceneManager"
+export * from "./core/component"
+export * from "./core/input"
+export * from "./core/animator"
+export * from "./core/assetLoader"
+
+// entities
+export * from "./entities/boxComponent"
+export * from "./entities/circleComponent"
+export * from "./entities/lineComponent"
+export * from "./entities/textComponent"
+export * from "./entities/spriteComponent"
+export * from "./entities/buttonComponent"
